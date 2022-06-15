0bad09d2b66d05b35df071d8453df9a2efe86896
cgyn <cgynlai@gmail.com>
2022-06-15 18:08:47 +0800

    1st commit

---
+++ src/main/java/com/cyl/mockito/test_doubles/fake/BookService.java
@@ -0,0 +1,9 @@
+package com.cyl.mockito.test_doubles.fake;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+}
